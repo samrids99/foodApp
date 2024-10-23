6d131adc966b361f31fbcc702851d1ab4411e847
samrids99 <samridsdale108@gmail.com>
2024-10-23 13:18:00 +0100

    app creation, tab ands navigation implemented

---
+++ src/app/pages/tabs/tabs.page.ts
@@ -0,0 +1,45 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonTabs, IonTab, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/angular/standalone';
+import { search, home, person, cart } from 'ionicons/icons';
+import { addIcons } from 'ionicons';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-tabs',
+  templateUrl: './tabs.page.html',
+  styleUrls: ['./tabs.page.scss'],
+  standalone: true,
+  imports: 
+  [
+    IonIcon, 
+    IonTabButton, 
+    IonTabBar, 
+    IonTab, 
+    IonTabs, 
+    IonContent, 
+    IonHeader, 
+    IonTitle, 
+    IonToolbar, 
+    CommonModule, 
+    FormsModule,
+    IonLabel
+  ]
+})
+export class TabsPage implements OnInit {
+
+  private router = inject(Router);
+
+  constructor() {
+    addIcons({home,search,cart,person});
+   }
+
+  ngOnInit() {
+  }
+
+  goHome() {
+    this.router.navigate(['/tabs/home']);
+  }
+
+}
---
+++ src/app/pages/tabs/tabs.routes.ts
@@ -0,0 +1,36 @@
+import { Routes } from '@angular/router';
+import { TabsPage } from '../tabs/tabs.page';
+
+export const routes: Routes = [
+  {
+    path: 'tabs',
+    component: TabsPage,
+    children: [
+      {
+        path: 'home',
+        loadComponent: () =>
+          import('../tabs/home/home.page').then((m) => m.HomePage),
+      },
+      {
+        path: 'search',
+        loadComponent: () =>
+          import('../tabs/search/search.page').then((m) => m.SearchPage),
+      },
+      {
+        path: 'cart',
+        loadComponent: () =>
+          import('../tabs/cart/cart.page').then((m) => m.CartPage),
+      },
+      {
+        path: 'account',
+        loadComponent: () =>
+          import('../tabs/account/account.page').then((m) => m.AccountPage),
+      },
+    ],
+  },
+  {
+    path: '',
+    redirectTo: 'tabs',
+    pathMatch: 'full',
+  },
+];
